21c1de37e12ae796ac070499602b2dbb94f30e23
yjjo8136 <yjun8136@gmail.com>
2025-03-05 12:46:29 +0900

    first commit

---
+++ src/main/java/com/kunnect/KUnnect/KUnnectApplication.java
@@ -0,0 +1,13 @@
+package com.kunnect.KUnnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KUnnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KUnnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kunnect/KUnnect/KUnnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kunnect.KUnnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KUnnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
